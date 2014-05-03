b28282e3b21b11192f7891f642f7b738d5de90a5
Jim Schmid <jim.schmid@1up.io>
2014-05-03 18:40:44 +0200

    Restructure everything.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var engines = require("consolidate");
+
+// configure application
+app.set("views", __dirname + "/views");
+app.engine("html", engines.ejs);
+app.set("view engine", "html");
+app.use(express.static(__dirname + "/node_modules"));
+app.use(express.static(__dirname + "/resources"));
+
+server.listen(8090);
+
+// routes
+app.get("/", function(req, res) {
+    res.render("index");
+});
