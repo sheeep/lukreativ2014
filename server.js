f52a8151e4f14dce25491fddaba74a4149623e41
Jim Schmid <jim.schmid@1up.io>
2014-05-29 19:36:06 +0200

    Finalized server.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
 var engines = require("consolidate");
 
 app.set("views", __dirname + "/views");
@@ -17,4 +18,80 @@ app.get("/", function(req, res) {
 
 app.get("/controller", function(req, res) {
     res.render("controller");
+});
+
+app.get("/thanks", function(req, res) {
+    res.render("thanks");
+});
+
+/**
+ * Game related stuff, mostly socket
+ * handler and helpers
+ */
+Game = {};
+Game.display = null;
+Game.controllers = {};
+
+io.sockets.on("connection", function(socket) {
+    socket.on("snd.register-display", function() {
+        if (null === Game.display) {
+            Game.display = socket;
+            Game.display.emit("rcv.register-display", {
+                success: true
+            });
+
+            return;
+        }
+
+        // if already a display attached
+        socket.emit("rcv.register-display", {
+            success: false,
+            message: "Display already registered"
+        });
+    });
+
+    socket.on("snd.register-controller", function() {
+        if (null === Game.display) {
+            socket.emit("rcv.register-controller", {
+                success: false,
+                message: "No display registered"
+            });
+
+            return;
+        }
+
+        Game.controllers[socket.id] = socket;
+        Game.display.emit("rcv.new-controller", {
+            id: socket.id
+        });
+
+    });
+
+    socket.on("snd.controller-data", function(data) {
+        Game.display.emit("rcv.controller-data", {
+            id: socket.id,
+            direction: data.direction
+        });
+    });
+
+    socket.on("disconnect", function() {
+        // Case 1: It was a controller
+        if (socket.id in Game.controllers) {
+            delete Game.controllers[socket.id];
+        }
+
+        // Case 2: No display attached
+        if (null === Game.display) {
+            return;
+        }
+
+        // Case 3: It was the display!
+        if (Game.display.id === socket.id) {
+            for (var i in Game.controllers) {
+                Game.controllers[sock].emit("rcv.display-disconnect");
+            }
+
+            return;
+        }
+    });
 });
